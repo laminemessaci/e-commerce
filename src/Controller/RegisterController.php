b0edc8311bc1054e61ce7b52589fed91e2142dc3
lamine messaci <lamine.messaci18@gmail.com>
2020-11-12 10:19:15 +0100

    Configure page register and use validator form

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class RegisterController extends AbstractController
+{
+    private $manager;
+    public function __construct(EntityManagerInterface $manager){
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("/inscription", name="register")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            //dd($user);
+            $passwordCrypte = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($passwordCrypte);
+            $this-> manager ->persist($user);
+            $this->manager->flush();
+
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
