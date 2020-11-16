d3235497eae68e5ea6efa67128082b45290fff80
lamine messaci <lamine.messaci18@gmail.com>
2020-11-16 19:42:48 +0100

    Configure EasyAdmin to upload image with EventSubscriber(EventSubscriberInterface)

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+
+    private $appKernel;
+
+    /**
+     * EasyAdminSubscriber constructor.
+     * @param $appKernel
+     */
+    public function __construct(KernelInterface $appKernel)
+    {
+        $this->appKernel = $appKernel;
+    }
+
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['setIllustration']
+        ];
+
+    }
+    public  function setIllustration(BeforeEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+        $tmp_name = $entity->getIllustration();
+        $fileName = uniqid();
+        $extention = pathinfo($_FILES['Product']['name']['illustration'], PATHINFO_EXTENSION);// png, jpg ...etc
+        //dd($extention);
+
+        $project_dir =  $this->appKernel->getProjectDir();
+        move_uploaded_file($tmp_name, $project_dir.'/public/uploads/'.$fileName.'.'.$extention);
+        $entity->setIllustration($fileName.'.'.$extention);
+    }
+}
